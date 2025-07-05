f77db7dc2e225f79c39c2782ece6b55132e5c61a
Pete Thorne <pete.thorne@learningpool.com>
2025-07-05 09:38:43 +0100

    Removed FAB

--- src/components/CalendarFooter.tsx
+++ src/components/CalendarFooter.tsx
@@ -34,27 +34,6 @@ export function CalendarFooter({
         </p>
         {children}
       </div>
-
-      {/* Floating Jump to Today Button - Desktop only */}
-      <button
-        onClick={onJumpToToday}
-        className="hidden sm:block fixed bottom-6 right-6 bg-blue-600 hover:bg-blue-700 text-white p-4 rounded-full shadow-lg transition-colors no-print z-50"
-        title="Jump to Today"
-      >
-        <svg
-          className="w-6 h-6"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 002 2z"
-          />
-        </svg>
-      </button>
     </>
   )
 }
